8d783fbaf60129997e6fb3951c7f430864c149d0
mohammed.eid <mohammed.eid@naseej.com>
2024-07-08 03:14:19 +0300

    update the project

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -18,9 +18,11 @@ export default function EditInvoiceForm({
   invoice: InvoiceForm;
   customers: CustomerField[];
 }) {
+  const initialState = { message: null, errors: {} };
   const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
+  const [state, formAction] = useActionState(updateInvoiceWithId, initialState);
   return (
-    <form action={updateInvoiceWithId}>
+    <form action={formAction}>
       <input type="hidden" name="id" value={invoice.id} />
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
@@ -124,3 +126,7 @@ export default function EditInvoiceForm({
     </form>
   );
 }
+function useActionState(updateInvoiceWithId: (prevState: import("@/app/lib/actions").State, formData: FormData) => Promise<{ errors: { customerId?: string[] | undefined; amount?: string[] | undefined; status?: string[] | undefined; }; message: string; } | { message: string; errors?: undefined; }>, initialState: { message: null; errors: {}; }): [any, any] {
+  throw new Error('Function not implemented.');
+}
+
